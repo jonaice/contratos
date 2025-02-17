030ab098d95140b5b1d1d9664406b887c45c0a69
Jonatan Eduardo Ramirez Lopez <yonylpz01@hotmail.com>
2025-02-16 21:08:25 -0600

    pagina de registro, diseño terminado.

--- app/auth/register/components/form.tsx
+++ app/auth/register/components/form.tsx
@@ -5,22 +5,28 @@ const FormRegister = () => {
 
     return (
 
-        <div className="w-full max-w-md">
+        <div className="w-full max-w-3xl">
             <h2 className="text-2xl font-semibold mb-5 text-primary"> Crear Cuenta</h2>
             <p className="text-gray-500">Ingresa tus datos para registrarte y comenzar a usar DocsFlex de manera fácil y segura.</p>
 
-            <form className="w-full mt-5">
+            <form className="w-full mt-5 grid lg:grid-cols-2 gap-4 md:grid-cols-1">
 
+                <InputString type="text" placeholder="Nombres" />
+                <InputString type="text" placeholder="Apellido Paterno" />
+                <InputString type="text" placeholder="Apellido Materno" />
+                <InputString type="text" placeholder="Número de teléfono" />
                 <InputString type="email" placeholder="Email" />
                 <InputString type="password" placeholder="Contraseña" />
+                <InputString type="password" placeholder="Confirmar Contraseña" />
             </form>
-            <div className="flex justify-end mb-5">
-                <button type="button" className="text-blue-800 duration-300">¿Olvidaste tu contraseña?</button>
+            <div className="w-full grid  grid-cols-2 mt-4">
+                <div className="col-start-2">
+                    <Button type="submit" label="Registrarse"/>
+                </div>
             </div>
-            <Button type="submit" label="Ingresar"/>
-            <div className="flex mt-10 justify-center">
-                <p className="text-gray-500">¿No tienes una cuenta?</p>
-                <button className="ml-1 font-bold text-primary hover:text-blue-800 transition-colors duration-300">Regístrate aquí</button>
+            <div className="flex mt-6 justify-center">
+                <p className="text-gray-500">¿Ya tienes una cuenta?</p>
+                <button className="ml-1 font-bold text-primary hover:text-blue-800 transition-colors duration-300">Inicia sesión aquí</button>
             </div>
         </div>
 
--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -7,7 +7,7 @@ export default function Login()
 
         <section className="h-full flex flex-col items-center justify-center">
 
-            <div className=" flex pb-12 -mt-10">
+            <div className=" flex pb-12">
                 <LogoLite />
             </div>
             <FormRegister />
